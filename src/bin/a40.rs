5a6d27c9bd8ab59bf6da07e606eaf472568b33d9
kibethh <kibethh@gmail.com>
2022-11-04 12:45:22 +0300

    144

--- src/bin/a40.rs
+++ src/bin/a40.rs
@@ -21,8 +21,74 @@
 // * Test your program by changing the vehicle status from both a storefront
 //   and from corporate
 
-struct Corporate;
+use std::cell::RefCell;
+use std::rc::Rc;
 
-struct StoreFront;
+#[derive(Debug)]
+enum Vehicle {
+    Car,
+    Truck,
+}
+#[derive(Debug, Hash, PartialEq, PartialOrd)]
+enum Status {
+    Available,
+    Unavailable,
+    Rented,
+    Maintenance,
+}
+
+#[derive(Debug)]
+struct Rental {
+    status: Status,
+    vehicle: Vehicle,
+    vin: String,
+}
+struct Corporate(Rc<RefCell<Vec<Rental>>>);
+
+struct StoreFront(Rc<RefCell<Vec<Rental>>>);
 
 fn main() {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn update_status() {
+        let vehicles = vec![
+            Rental {
+                status: Status::Available,
+                vehicle: Vehicle::Car,
+                vin: "123".to_owned(),
+            },
+            Rental {
+                status: Status::Maintenance,
+                vehicle: Vehicle::Car,
+                vin: "abc".to_owned(),
+            },
+        ];
+        let vehicles = Rc::new(RefCell::new(vehicles));
+        let corporate = Corporate(Rc::clone(&vehicles));
+        let storefront = Corporate(Rc::clone(&vehicles));
+
+        {
+            let mut rentals = storefront.0.borrow_mut();
+            if let Some(car) = rentals.get_mut(0) {
+                assert_eq!(car.status, Status::Available);
+                car.status = Status::Rented
+            }
+        }
+        {
+            let mut rentals = corporate.0.borrow_mut();
+            if let Some(car) = rentals.get_mut(0) {
+                assert_eq!(car.status, Status::Rented);
+                car.status = Status::Available
+            }
+        }
+
+        let rentals = storefront.0.borrow();
+        if let Some(car) = rentals.get(0) {
+            assert_eq!(car.status, Status::Available);
+        }
+    }
+}
